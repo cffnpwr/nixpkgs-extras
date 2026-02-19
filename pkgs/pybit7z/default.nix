60fd726ac9c02445eeecc9314b2bbc91ee664bec
cffnpwr <cffnpwr@gmail.com>
2026-02-19 17:12:54 +0900

    feat: :sparkles: pybit7zのupdateScriptを追加

--- pkgs/pybit7z/default.nix
+++ pkgs/pybit7z/default.nix
@@ -1,48 +1,26 @@
 {
   lib,
+  pkgs,
   stdenv,
   buildPythonPackage,
   fetchurl,
   autoPatchelfHook,
   importlib-metadata,
+  writeShellScript,
 }:
 
 let
-  version = "0.4.0";
+  source = builtins.fromJSON (builtins.readFile ./source.json);
 
-  wheels = {
-    x86_64-linux = {
-      filename = "pybit7z-${version}-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
-      hash = "sha256-0gaZZqJsL8uSRHXvtHuO3H3xaoR/KhngoUAV9S3hcAo=";
-      url = "https://files.pythonhosted.org/packages/d0/f1/ff756b4561dcabc6917b8195d3d5248b2b52488a536c493e32c950f001ed/pybit7z-${version}-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
-    };
-    aarch64-linux = {
-      filename = "pybit7z-${version}-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.whl";
-      hash = "sha256-r2LYj3VzEvYWIa6NWdEGm2eo3p69JO2I78BzUvdgtCU=";
-      url = "https://files.pythonhosted.org/packages/62/da/b1f7f8444acab3ae27592d8ddcd64899c0b967448dc08debade1aac7c539/pybit7z-${version}-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.whl";
-    };
-    x86_64-darwin = {
-      filename = "pybit7z-${version}-cp313-cp313-macosx_10_15_x86_64.whl";
-      hash = "sha256-5401EbCeLehAcyKpPDz5F9eDXB2VpLYJW2Qcdr59hag=";
-      url = "https://files.pythonhosted.org/packages/87/da/976f1dfdaf7dd398fc0d4905a51b1859841fc2a7d8ccf72b980e644a7ee5/pybit7z-${version}-cp313-cp313-macosx_10_15_x86_64.whl";
-    };
-    aarch64-darwin = {
-      filename = "pybit7z-${version}-cp313-cp313-macosx_11_0_arm64.whl";
-      hash = "sha256-ikWD34VvgLP1HoqFa/otcvkiah63OYvoA0MNOIuSPik=";
-      url = "https://files.pythonhosted.org/packages/7e/ba/daf2d3df0a93dca35dbb536ca09129e663d43a0104b5318b236c9337885b/pybit7z-${version}-cp313-cp313-macosx_11_0_arm64.whl";
-    };
-  };
-
-  wheel = wheels.${stdenv.hostPlatform.system};
+  wheel = source.wheels.${stdenv.hostPlatform.system};
 in
 buildPythonPackage {
   pname = "pybit7z";
-  inherit version;
+  inherit (source) version;
   format = "wheel";
 
   src = fetchurl {
     inherit (wheel) url hash;
-    name = wheel.filename;
   };
 
   nativeBuildInputs = lib.optionals stdenv.isLinux [ autoPatchelfHook ];
@@ -50,6 +28,10 @@ buildPythonPackage {
   dependencies = [ importlib-metadata ];
 
   pythonImportsCheck = [ "pybit7z" ];
+
+  passthru.updateScript = writeShellScript "pybit7z-update-script" ''
+    exec ${pkgs.python3}/bin/python3 ${./update.py} "$@"
+  '';
 
   meta = {
     description = "Python bindings for bit7z, a C++ static library offering 7-zip compression/extraction";
